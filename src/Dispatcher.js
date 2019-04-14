44c70b8b02c83e22557a7ef7e884cd427d708848
conni <c.buschka@arcusx.com>
2019-04-14 15:58:47 +0200

    Fixed invalid import.

--- src/Dispatcher.js
+++ src/Dispatcher.js
@@ -1,5 +1,5 @@
 import {EventEmitter} from 'events';
-import setImmediate from 'setimmediate';
+import {setImmediate} from 'setimmediate';
 
 function upperFirst(s) {
     return s.charAt(0).toUpperCase() + s.slice(1);
